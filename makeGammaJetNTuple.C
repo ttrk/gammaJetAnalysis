a8f93327e6508c46fb5198b2a644fb941ca6a236
R. Alex Barbieri <luck@mit.edu>
2013-07-14 09:28:55 -0400

    change leading jets to inclusive jets

--- makeGammaJetNTuple.C
+++ makeGammaJetNTuple.C
@@ -15,19 +15,18 @@ void makeGammaGetNTuple()
 { 
   TString inFile;
 
-  // files[0] = "/mnt/hadoop/cms/store/user/luck/pA2013_forests/PA2013_HiForest_PromptReco_HLT_Photon40.root";
-  // files[1] = "/mnt/hadoop/cms/store/user/luck/pA2013_MC/HiForest2_QCDPhoton30_5020GeV_100k.root";
-
-  inFile = "/mnt/hadoop/cms/store/user/luck/PA2013_pyquen_allQCDPhoton_forest_v85/pA_Pyquen_allQCDPhoton280_hiForest2_v85.root";
-  bool montecarlo = true;
+  // inFile = "/mnt/hadoop/cms/store/user/luck/PA2013_pyquen_allQCDPhoton_forest_v85/pA_Pyquen_allQCDPhoton280_hiForest2_v85.root";
+  inFile = "/mnt/hadoop/cms/store/user/luck/pA_photonSkimForest_v85/pA_photonSkimForest_v85.root";
+  
+  bool montecarlo = false;
 
   TString name;
-  name = "gammaJets_inclusive_dphi7pi8_allQCDPhoton280_v3.root";
+  name = "gammaJets_inclusive_dphi7pi8_data.root";
 
   TFile *outfile = new TFile(name,"RECREATE");
 
   TString varList;
-  varList = "gPt:gEta:gPhi:jPt:jEta:jPhi:HF:HFeta4:avgEta:dPhi:cc4:cr4:ct4PtCut20:hadronicOverEm:sigmaIetaIeta:run:r9";
+  varList = "gPt:gEta:gPhi:jPt:jEta:jPhi:HF:HFeta4:avgEta:dPhi:cc4:cr4:ct4PtCut20:hadronicOverEm:sigmaIetaIeta:run:r9:event";
   if(montecarlo)
     varList += ":genMomId:genCalIsoDR04:genTrkIsoDR04:ptHat:matchedGPt:matchedJPt";
   
@@ -47,7 +46,14 @@ void makeGammaGetNTuple()
     c->GetEntry(jentry);
 
     //event selection
-    if( !((montecarlo || c->skim.pHBHENoiseFilter) && c->skim.phfPosFilter1 && c->skim.phfNegFilter1 && c->skim.phltPixelClusterShapeFilter && c->skim.pprimaryvertexFilter) )
+    if( !(
+	  (montecarlo || c->skim.pHBHENoiseFilter)
+	  && c->skim.phfPosFilter1
+	  && c->skim.phfNegFilter1
+	  && c->skim.phltPixelClusterShapeFilter
+	  && c->skim.pprimaryvertexFilter
+	  )
+      )
       continue;
 
     if(c->photon.nPhotons == 0)
@@ -69,64 +75,67 @@ void makeGammaGetNTuple()
       continue;
 
     //loop over 'away' jets
-    int jet2index = -1;
-    double jet2pt = 0;
+    //int jet2index = -1;
+    //double jet2pt = 0;
     for(int i = 0; i<c->akPu3PF.nref; i++)
     {
       if( TMath::Abs(c->akPu3PF.jteta[i]) > 3.0)
 	continue;
 	
-      Double_t dphi = TMath::ACos(TMath::Cos(c->photon.phi[leadingIndex] - c->akPu3PF.jtphi[i]));
+      Double_t dphi =
+	TMath::ACos(TMath::Cos(c->photon.phi[leadingIndex]
+			       - c->akPu3PF.jtphi[i]));
       if( dphi < 7.*TMath::Pi()/8. )
 	continue;
 	
-      if(c->akPu3PF.jtpt[i] > jet2pt)
+      //   if(c->akPu3PF.jtpt[i] > jet2pt)
+      //   {
+      // 	jet2pt = c->akPu3PF.jtpt[i];
+      // 	jet2index = i;
+      //   }
+      // }
+
+      // if(jet2index == -1)
+      //   continue;
+
+      //Double_t dphi = TMath::ACos(TMath::Cos(c->photon.phi[leadingIndex] - c->akPu3PF.jtphi[i]));
+
+      Float_t gPt = c->photon.pt[leadingIndex];
+      Float_t gEta = c->photon.eta[leadingIndex];
+      Float_t gPhi = c->photon.phi[leadingIndex];
+      Float_t jPt = c->akPu3PF.jtpt[i];
+      Float_t jEta = c->akPu3PF.jteta[i];
+      Float_t jPhi = c->akPu3PF.jtphi[i];
+      Float_t HF = c->evt.hiHF;
+      Float_t HFeta4 = c->evt.hiHFplusEta4 + c->evt.hiHFminusEta4;
+      Float_t avgEta = (c->photon.eta[leadingIndex] + c->akPu3PF.jteta[i])/2.0;
+      Float_t dPhi = dphi;
+      Float_t cc4 = c->photon.cc4[leadingIndex];
+      Float_t cr4 = c->photon.cr4[leadingIndex];
+      Float_t ct4PtCut20 = c->photon.ct4PtCut20[leadingIndex];
+      Float_t hadronicOverEm = c->photon.hadronicOverEm[leadingIndex];
+      Float_t sigmaIetaIeta = c->photon.sigmaIetaIeta[leadingIndex];
+      Float_t run = c->evt.run;
+      Float_t r9 = c->photon.r9[leadingIndex];
+      Float_t event = c->photon.eventt;
+
+      if(!montecarlo)
       {
-	jet2pt = c->akPu3PF.jtpt[i];
-	jet2index = i;
+	Float_t x[] = {gPt,gEta,gPhi,jPt,jEta,jPhi,HF,HFeta4,avgEta,dPhi,cc4,cr4,ct4PtCut20,hadronicOverEm,sigmaIetaIeta,run,r9,event};
+	outTuple->Fill(x);
       }
-    }
+      else
+      {
+	Float_t genMomId = c->photon.genMomId[leadingIndex];
+	Float_t genCalIsoDR04 = c->photon.genCalIsoDR04[leadingIndex];
+	Float_t genTrkIsoDR04 = c->photon.genTrkIsoDR04[leadingIndex];
+	Float_t ptHat = c->photon.ptHat;
+	Float_t matchedGPt = c->photon.genMatchedPt[leadingIndex];
+	Float_t matchedJPt = c->akPu3PF.matchedPt[i];
 
-    if(jet2index == -1)
-      continue;
-
-    Double_t dphi = TMath::ACos(TMath::Cos(c->photon.phi[leadingIndex] - c->akPu3PF.jtphi[jet2index]));
-
-    // gPt:gEta:gPhi:jPt:jEta:jPhi:HF:HFeta4:avgEta:dPhi:cc4:cr4:ct4PtCut20:hadronicOverEm:sigmaIetaIeta:run
-    Float_t gPt = c->photon.pt[leadingIndex];
-    Float_t gEta = c->photon.eta[leadingIndex];
-    Float_t gPhi = c->photon.phi[leadingIndex];
-    Float_t jPt = c->akPu3PF.jtpt[jet2index];
-    Float_t jEta = c->akPu3PF.jteta[jet2index];
-    Float_t jPhi = c->akPu3PF.jtphi[jet2index];
-    Float_t HF = c->evt.hiHF;
-    Float_t HFeta4 = c->evt.hiHFplusEta4 + c->evt.hiHFminusEta4;
-    Float_t avgEta = (c->photon.eta[leadingIndex] + c->akPu3PF.jteta[jet2index])/2.0;
-    Float_t dPhi = dphi;
-    Float_t cc4 = c->photon.cc4[leadingIndex];
-    Float_t cr4 = c->photon.cr4[leadingIndex];
-    Float_t ct4PtCut20 = c->photon.ct4PtCut20[leadingIndex];
-    Float_t hadronicOverEm = c->photon.hadronicOverEm[leadingIndex];
-    Float_t sigmaIetaIeta = c->photon.sigmaIetaIeta[leadingIndex];
-    Float_t run = c->evt.run;
-    Float_t r9 = c->photon.r9[leadingIndex];
-
-    if(!montecarlo)
-    {
-      Float_t x[] = {gPt,gEta,gPhi,jPt,jEta,jPhi,HF,HFeta4,avgEta,dPhi,cc4,cr4,ct4PtCut20,hadronicOverEm,sigmaIetaIeta,run,r9};
-      outTuple->Fill(x);
-    }
-    else
-    {
-      Float_t genMomId = c->photon.genMomId[leadingIndex];
-      Float_t genCalIsoDR04 = c->photon.genCalIsoDR04[leadingIndex];
-      Float_t genTrkIsoDR04 = c->photon.genTrkIsoDR04[leadingIndex];
-      Float_t ptHat = c->photon.ptHat;
-      Float_t matchedGPt = c->photon.genMatchedPt[leadingIndex];
-      Float_t matchedJPt = c->akPu3PF.matchedPt[jet2index];
-
-      Float_t x[] = {gPt,gEta,gPhi,jPt,jEta,jPhi,HF,HFeta4,avgEta,dPhi,cc4,cr4,ct4PtCut20,hadronicOverEm,sigmaIetaIeta,run,r9,genMomId,genCalIsoDR04,genTrkIsoDR04,ptHat,matchedGPt,matchedJPt};
-      outTuple->Fill(x);
+	Float_t x[] = {gPt,gEta,gPhi,jPt,jEta,jPhi,HF,HFeta4,avgEta,dPhi,cc4,cr4,ct4PtCut20,hadronicOverEm,sigmaIetaIeta,run,r9,event,genMomId,genCalIsoDR04,genTrkIsoDR04,ptHat,matchedGPt,matchedJPt};
+	outTuple->Fill(x);
+      }
     }
 
   }
