c9cf8b5de4ac4800d40ea7cf6c1ed1d1d34270f8
R. Alex Barbieri <luck@mit.edu>
2013-07-14 11:09:19 -0400

    switch to mc

---
+++ uniquePhotons.C
@@ -0,0 +1,45 @@
+// uniquePhotons.C
+// plot values from the gamma Jet ntuple using only one entry per event
+// i.e. not nJets per event
+// inspired by http://stackoverflow.com/questions/12002098/removing-duplicate-entries-in-a-root-ttree
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TNtuple.h"
+#include "TEntryList.h"
+
+void uniquePhotons()
+{
+  TFile *inFile = TFile::Open("gammaJets_inclusive_dphi7pi8_data.root");
+  TNtuple *inTuple = (TNtuple*)inFile->Get("gammaJets");
+
+  set<int> eventIds; // keep track of already seen event numbers
+  int EVENT;
+  int nEntries = inTuple->GetEntries();
+
+  inTuple->SetBranchAddress("event",&EVENT); // grab the event number from the tree
+
+  TEntryList *tlist = new TEntryList(inTuple); // initialize entry list for 'TTree* tree'
+
+// loop over the entries in 'tree'
+  for (int j = 0; j < nEntries; ++j)
+  {
+    inTuple->GetEvent(j);
+
+// if we have not seen this event yet, add it to the set
+// and to the entry list
+    if (eventIds.count(EVENT) == 0)
+    {
+      eventIds.insert(EVENT);
+      tlist->Enter(j,inTuple);
+    }
+  }
+
+// apply the entry list to the tree
+  inTuple->SetEntryList(tlist);
+
+// histogram of the variable 'var' will be drawn only with the
+// entries specified by the entry list.
+  inTuple->Draw("gPt");
+
+}
