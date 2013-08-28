22ceaff0351ac44ff1a6181c70d965339eb14911
Yongsun <kingmking@gmail.com>
2013-08-28 11:07:16 +0900

    changed the pt binning

--- histogramProducer/runGammaJetHistProducer.sh
+++ histogramProducer/runGammaJetHistProducer.sh
@@ -4,30 +4,30 @@
 
 
 #pp data 
-#root -l -b -q 'gammaJetHistProducer.C+(2, 40, 50, 30, 4)'
-#root -l -b -q 'gammaJetHistProducer.C+(2, 50, 60, 30, 4)'
-#root -l -b -q 'gammaJetHistProducer.C+(2, 60, 80, 30, 4)'
-#root -l -b -q 'gammaJetHistProducer.C+(2, 80,120, 30, 4)'
-#root -l -b -q 'gammaJetHistProducer.C+(2, 120,999, 30, 4)'
+root -l -b -q 'gammaJetHistProducer.C+(2, 40, 50, 30, 4)'
+root -l -b -q 'gammaJetHistProducer.C+(2, 50, 60, 30, 4)'
+root -l -b -q 'gammaJetHistProducer.C+(2, 60, 80, 30, 4)'
+root -l -b -q 'gammaJetHistProducer.C+(2, 80,9999, 30, 4)'
+
 
 #PbPb data
-#root -l -b -q 'gammaJetHistProducer.C+(0, 40,50, 30, 10030)'
-#root -l -b -q 'gammaJetHistProducer.C+(0, 50,60, 30, 10030)'
-#root -l -b -q 'gammaJetHistProducer.C+(0, 60,80, 30, 10030)'
-#root -l -b -q 'gammaJetHistProducer.C+(0, 80,120, 30, 10030)'
-#root -l -b -q 'gammaJetHistProducer.C+(0, 120,9999, 30, 10030)'
+root -l -b -q 'gammaJetHistProducer.C+(0, 40,50, 30, 10030)'
+root -l -b -q 'gammaJetHistProducer.C+(0, 50,60, 30, 10030)'
+root -l -b -q 'gammaJetHistProducer.C+(0, 60,80, 30, 10030)'
+root -l -b -q 'gammaJetHistProducer.C+(0, 80,9999, 30, 10030)'
+
 
 #pPb data and MC
 
 for i in 4 5
 do
-    for icent in 1 2 3  
-    do
+    for icent in 1 2 3 
+   do
 	root -l -b -q 'gammaJetHistProducer.C++('$i', 40,50, 30, '$icent')'
 	root -l -b -q 'gammaJetHistProducer.C+('$i', 50,60, 30, '$icent')'
 	root -l -b -q 'gammaJetHistProducer.C+('$i', 60,80, 30, '$icent')'
-	root -l -b -q 'gammaJetHistProducer.C+('$i', 80,120, 30, '$icent')'
-	root -l -b -q 'gammaJetHistProducer.C+('$i', 120,9999, 30, '$icent')'
+	root -l -b -q 'gammaJetHistProducer.C+('$i', 80,9999, 30, '$icent')'
 	root -l -b -q 'gammaJetHistProducer.C+('$i', 40,9999, 30, '$icent')'
     done 
 done
+
