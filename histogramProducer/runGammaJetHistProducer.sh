adcd73f1922a42270d28c73aff6733757f32b617
Yongsun <kingmking@gmail.com>
2013-08-28 11:16:42 +0900

    changed pt, centrality bins

--- histogramProducer/runGammaJetHistProducer.sh
+++ histogramProducer/runGammaJetHistProducer.sh
@@ -8,26 +8,31 @@ root -l -b -q 'gammaJetHistProducer.C+(2, 40, 50, 30, 4)'
 root -l -b -q 'gammaJetHistProducer.C+(2, 50, 60, 30, 4)'
 root -l -b -q 'gammaJetHistProducer.C+(2, 60, 80, 30, 4)'
 root -l -b -q 'gammaJetHistProducer.C+(2, 80,9999, 30, 4)'
+root -l -b -q 'gammaJetHistProducer.C+(2, 40,9999, 30, 4)'
 
 
 #PbPb data
-root -l -b -q 'gammaJetHistProducer.C+(0, 40,50, 30, 10030)'
-root -l -b -q 'gammaJetHistProducer.C+(0, 50,60, 30, 10030)'
-root -l -b -q 'gammaJetHistProducer.C+(0, 60,80, 30, 10030)'
-root -l -b -q 'gammaJetHistProducer.C+(0, 80,9999, 30, 10030)'
+for icent in 10030 13099 10020 12050 15099
+do
+    root -l -b -q 'gammaJetHistProducer.C+(0, 40,50, 30, '$icent')'
+    root -l -b -q 'gammaJetHistProducer.C+(0, 50,60, 30,'$icent')'
+    root -l -b -q 'gammaJetHistProducer.C+(0, 60,80, 30, '$icent')'
+    root -l -b -q 'gammaJetHistProducer.C+(0, 80,9999, 30, '$icent')'
+    root -l -b -q 'gammaJetHistProducer.C+(0, 40,9999, 30, '$icent')'
+done
 
 
 #pPb data and MC
 
-for i in 4 5
-do
-    for icent in 1 2 3 
-   do
-	root -l -b -q 'gammaJetHistProducer.C++('$i', 40,50, 30, '$icent')'
-	root -l -b -q 'gammaJetHistProducer.C+('$i', 50,60, 30, '$icent')'
-	root -l -b -q 'gammaJetHistProducer.C+('$i', 60,80, 30, '$icent')'
-	root -l -b -q 'gammaJetHistProducer.C+('$i', 80,9999, 30, '$icent')'
-	root -l -b -q 'gammaJetHistProducer.C+('$i', 40,9999, 30, '$icent')'
-    done 
-done
+#for i in 4 5
+#do
+#    for icent in 1 2 3 
+#   do
+#	root -l -b -q 'gammaJetHistProducer.C++('$i', 40,50, 30, '$icent')'
+#	root -l -b -q 'gammaJetHistProducer.C+('$i', 50,60, 30, '$icent')'
+#	root -l -b -q 'gammaJetHistProducer.C+('$i', 60,80, 30, '$icent')'
+#	root -l -b -q 'gammaJetHistProducer.C+('$i', 80,9999, 30, '$icent')'
+#	root -l -b -q 'gammaJetHistProducer.C+('$i', 40,9999, 30, '$icent')'
+ #   done 
+#done
 
