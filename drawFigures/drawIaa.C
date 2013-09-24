a3df39237fcdec38c639b4c55d88799a8065e724
Yongsun <kingmking@gmail.com>
2013-09-24 16:24:28 +0900

    4 pt bins and 2 centrality bins

--- drawFigures/drawIaa.C
+++ drawFigures/drawIaa.C
@@ -6,7 +6,7 @@
 void drawIaa( bool saveFigures=true) {
 
     const int nPtBin = 4;
-    double ptBin[nPtBin+1] = {40, 50,60,80,9999}; 
+    double ptBin[nPtBin+1] = {40, 50,60,80,9999};
     double ptBinPaDraw[nPtBin+1] = { 40.5 ,49.5, 58.5,  76.5,  123. } ;
     // double AvePtBin[nPtBin+1] = { 45, 54.1479, 67.4204, 99.6956, 9999};
     
@@ -31,26 +31,26 @@ void drawIaa( bool saveFigures=true) {
     for (int ipt=1 ; ipt<=nPtBin ; ipt++) {
       for (int icoll=0 ; icoll<6 ; icoll++) {
 	TString sampleName = getSampleName( icoll ) ;
-	//	char* fname =  Form("ffFiles/photonTrackCorr_%s_output_photonPtThr%d_to_%d_jetPtThr30_20130923.root",sampleName.Data(), (int)ptBin[ipt-1], (int)ptBin[ipt]);
-	char* fname =  Form("ffFiles/photonTrackCorr_%s_output_photonPtThr%d_to_%d_jetPtThr30_20130923.root",sampleName.Data(), (int)ptBin[ipt-1], 9999);
+	//		char* fname =  Form("ffFiles/photonTrackCorr_%s_output_photonPtThr%d_to_%d_jetPtThr30_20130924.root",sampleName.Data(), (int)ptBin[ipt-1], (int)ptBin[ipt]);
+		char* fname =  Form("ffFiles/photonTrackCorr_%s_output_photonPtThr%d_to_%d_jetPtThr30_20130924.root",sampleName.Data(), (int)ptBin[ipt-1], 9999);
 	histFile[icoll][ipt] = new TFile(fname) ;
 	cout << " Reading file : " << fname << endl;
 	if ( histFile[icoll][ipt]->IsZombie()  == false ) {
 	  cout << " Success." << endl;
 	  if ( (icoll == kPPDATA) || (icoll == kPPMC) )   {  //  PP
 	    int icent = 7 ; 
-	    hJetPt[icoll][icent][ipt] = (TH1D*)histFile[icoll][ipt]->Get(Form("jetPt_icent%d_final", icent)) ;
+	    hJetPt[icoll][icent][ipt] = (TH1D*)histFile[icoll][ipt]->Get(Form("jetPtForIaa_icent%d_final", icent)) ;
 	    cout << " Getting histogram : " << Form("jetPt_icent%d_final", icent) << endl;
-	    hJetPt[icoll][7][ipt]->Rebin(10);
-	    hJetPt[icoll][7][ipt]->Scale(1./10);
+	    //	    hJetPt[icoll][7][ipt]->Rebin(10);
+	    // hJetPt[icoll][7][ipt]->Scale(1./10);
 	    
 	  }
 	  if ( ( icoll == kHIDATA) ||  (icoll == kHIMC) ) { // PbPb
 	    for ( int icent = 1; icent <= nCentBinHI ; icent++ ) {
-	      hJetPt[icoll][icent][ipt] = (TH1D*)histFile[icoll][ipt]->Get(Form("jetPt_icent%d_final", centBinHI[icent] ) );
+	      hJetPt[icoll][icent][ipt] = (TH1D*)histFile[icoll][ipt]->Get(Form("jetPtForIaa_icent%d_final", centBinHI[icent] ) );
 	      cout << " Getting histogram : " << Form("jetPt_icent%d_final", centBinHI[icent] ) << endl;
-	      hJetPt[icoll][icent][ipt]->Rebin(10);
-	      hJetPt[icoll][icent][ipt]->Scale(1/10.);
+	      //	      hJetPt[icoll][icent][ipt]->Rebin(10);
+	      //hJetPt[icoll][icent][ipt]->Scale(1/10.);
 	    }
 	  }
 	}
@@ -70,7 +70,7 @@ void drawIaa( bool saveFigures=true) {
     
     TH1D* hTempPt = new TH1D("hTemp",";p_{T}^{#gamma} (GeV);",200,10,300);
     
-    TCanvas* c2 = new TCanvas("c2","",1200,700);
+    TCanvas* c2 = new TCanvas("c2","",1000,450);
     makeMultiPanelCanvas(c2,nPtBin,2,0.0,0.0,0.2,0.15,0.02);
     for ( int ipt = 1 ; ipt<=nPtBin  ; ipt++) {
 
@@ -78,10 +78,10 @@ void drawIaa( bool saveFigures=true) {
 
         // draw pp
         hTempPt->SetXTitle("p_{T}^{Jet} (GeV)");
-        hTempPt->SetYTitle("#frac{dN}{dp_{T}} #frac{1}{N}");
-        hTempPt->SetAxisRange(10,150,"X");
+        hTempPt->SetYTitle("#frac{dN}{dp_{T}} #frac{1}{N^{#gamma}}");
+        hTempPt->SetAxisRange(30,200,"X");
 	//        hTempPt->SetAxisRange(0,0.025,"Y");
-	hTempPt->SetAxisRange(0.001,0.08,"Y");
+	hTempPt->SetAxisRange(0.00007,0.11,"Y");
         handsomeTH1(hTempPt,0);
         hTempPt->DrawCopy();
 	gPad->SetLogy();
@@ -95,29 +95,33 @@ void drawIaa( bool saveFigures=true) {
         for ( int icent = 1; icent <= nCentBinHI ; icent++ ) {
             handsomeTH1(hJetPt[kHIDATA][icent][ipt],kRed);
 	    if ( icent == 2 ) hJetPt[kHIDATA][icent][ipt]->SetMarkerStyle(24);
-            //      if ( icent == 1 )       hJetPt[kHIDATA][icent][ipt]->Draw("same");
-            if ( icent == 1 ) hJetPt[kHIDATA][icent][ipt]->Draw("same");
+	    hJetPt[kHIDATA][icent][ipt]->Draw("same");
         }
        
         double dx1=0.15;
-        if ( ipt == nPtBin ) 
-            drawText(Form("p_{T}^{#gamma} < %dGeV, ", (int)ptBin[ipt-1]), 0.12+dx1+0.25,0.85,1,15);//yeonju 130823
-        else
-            drawText(Form("%dGeV < p_{T}^{#gamma} < %dGeV, ", (int)ptBin[ipt-1], (int)ptBin[ipt]), 0.12+dx1,0.85,1,15);//yeonju 130823
 
+	drawText(Form("p_{T}^{#gamma} > %dGeV, ", (int)ptBin[ipt-1]), 0.12+dx1+0.25,0.85,1,15);
+	/*
+	  if ( ipt == nPtBin ) 
+	  drawText(Form("p_{T}^{#gamma} > %dGeV, ", (int)ptBin[ipt-1]), 0.12+dx1+0.25,0.85,1,15);
+	  else
+	  drawText(Form("%dGeV < p_{T}^{#gamma} < %dGeV, ", (int)ptBin[ipt-1], (int)ptBin[ipt]), 0.12+dx1,0.85,1,15);
+	*/
+	
         onSun(30,0,200,0);
 
         c2->cd(ipt + nPtBin);
-	hTempPt->SetAxisRange(0,2,"Y");
+	hTempPt->SetAxisRange(0,2.5,"Y");
 	hTempPt->SetYTitle("Jet I_{AA}");
 	handsomeTH1(hTempPt,0);
         hTempPt->DrawCopy();
                                                                                                                
         for ( int icent = 1; icent <= nCentBinHI ; icent++ ) {
-	  handsomeTH1(hJetPt[kHIDATA][icent][ipt],kRed);
+	  handsomeTH1(hIaa[icent][ipt],kRed);
 	  if ( icent == 2 )         hIaa[icent][ipt]->SetMarkerStyle(24);
-	  if ( icent == 1 )   
+	  // if ( icent == 1 )   
 	    hIaa[icent][ipt]->Draw("same");
+	  jumSun(30,1,200,1);
         }
 	
 	
@@ -128,14 +132,14 @@ void drawIaa( bool saveFigures=true) {
 
     c2->cd(1);
     
-    TLegend *l2 = new TLegend(0.2116935,0.7012712,0.6149194,0.904661,NULL,"brNDC");
+    TLegend *l2 = new TLegend(0.5933981,0.3877892,0.9981436,0.7857262,NULL,"brNDC");
     easyLeg(l2,"2.76TeV");
-//    l2->AddEntry(hDphi[kPPDATA][7][1],"pp ","p");
-    //   l2->AddEntry(hDphi[kHIDATA][1][1],"PbPb 30-100%","p");
-    //  l2->AddEntry(hDphi[kHIDATA][2][1],"PbPb 0-30%","p");
-    
-    if (saveFigures)   c2->SaveAs("figures/pT_dependence_jetPt_pp_pbpb_figure1.pdf");
-    if (saveFigures)   c2->SaveAs("figures/pT_dependence_jetPt_pp_pbpb_figure1.gif");
+    l2->AddEntry(hJetPt[kPPDATA][7][1],"pp", "p");
+    l2->AddEntry(hJetPt[kHIDATA][2][1],"PbPb 30-100%", "p");
+    l2->AddEntry(hJetPt[kHIDATA][1][1],"PbPb 0-30%", "p");
+    l2->Draw();
+    if (saveFigures)   c2->SaveAs("figures/pT_dependence_IAA_figure1.pdf");
+    if (saveFigures)   c2->SaveAs("figures/pT_dependence_IAA_figure1.gif");
 
     
     
