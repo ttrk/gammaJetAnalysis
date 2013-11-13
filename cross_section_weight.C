128c247c7c7d067d3bff86639594f135931a6597
Yongsun Kim <jazzitup@hidsk0001.cmsaf.mit.edu>
2013-11-13 01:49:45 -0500

    added number of event counts

--- cross_section_weight.C
+++ cross_section_weight.C
@@ -1,3 +1,11 @@
+///////////////////////////////////////////////////////////////////                        
+// cross_section_weight.C                                        //
+// Creator : Yongsun Kim (Korea U.), kingmking@gmail.com         //
+// Function : Calculate the relative cross-section of each pt hat//
+// samples                                                       //
+///////////////////////////////////////////////////////////////////                        
+///////////////////////////////////////////////////////////////////     
+
 #include "TFile.h"
 #include "TTree.h"
 #include "TNtuple.h"
@@ -21,11 +29,56 @@
 
 void cross_section_weight() 
 {
+  
+  TFile *f1[10];
+  TTree* t1[10];
+  double n1[10];
+  double n2[10][10];
+
+    cout << endl << " Entries : " << endl;
+  for ( int i=1 ; i <=5; i++) {
+    if ( i==1 )  f1[i] = new TFile("/mnt/hadoop/cms/store/user/luck/PbPb_pythiaHYDJET_forest_AllQCDPhotons/PbPb_pythiaHYDJET_forest_AllQCDPhotons30.root");
+    else if ( i==2 )  f1[i] = new TFile("/mnt/hadoop/cms/store/user/luck/PbPb_pythiaHYDJET_forest_AllQCDPhotons/PbPb_pythiaHYDJET_forest_AllQCDPhotons50.root");
+    else if ( i==3 )  f1[i] = new TFile("/mnt/hadoop/cms/store/user/luck/PbPb_pythiaHYDJET_forest_AllQCDPhotons/PbPb_pythiaHYDJET_forest_AllQCDPhotons80.root");
+    else if ( i==4 )  f1[i] = new TFile("/mnt/hadoop/cms/store/user/luck/PbPb_pythiaHYDJET_forest_AllQCDPhotons/PbPb_pythiaHYDJET_forest_AllQCDPhotons120.root");
+    else if ( i==5 )  f1[i] = new TFile("/mnt/hadoop/cms/store/user/luck/PbPb_pythiaHYDJET_forest_AllQCDPhotons/PbPb_pythiaHYDJET_forest_AllQCDPhotons170.root");
+
+
+    t1[i] = (TTree*)f1[i]->Get("multiPhotonAnalyzer/photon");
+    n1[i] = t1[i]->GetEntries();
+    for ( int j=1 ; j<=5; j++) { 
+      
+      int pt1(0), pt2(0);
+      if ( j==1 ) pt1 = 30 ;     if ( j==2) pt1 =50 ;     if ( j==3) pt1 =80 ;     if ( j==4) pt1 =120 ;     if ( j==5) pt1 = 170 ;
+      if ( j==1 ) pt2 = 50 ;     if ( j==2) pt2 =80 ;     if ( j==3) pt2 =120 ;     if ( j==4) pt2 =170 ;     if ( j==5) pt2 = 10000 ;
+      n2[i][j] = t1[i]->GetEntries(Form("ptHat>%d && ptHat<%d", pt1, pt2) );
+
+      cout << Form("file of %dth,   pt hat : %d - %d : %d", i,  pt1, pt2, (int)n2[i][j]) << endl;
+    }
+  }
+  cout << " ===============================" << endl;
+  
+  double n3[10];
+  for ( int j=1 ; j<=5; j++) {
+    n3[j] = n2[1][j] + n2[2][j] + n2[3][j] + n2[4][j] + n2[5][j] ;
+    int pt1(0), pt2(0);
+    if ( j==1 ) pt1 = 30 ;     if ( j==2) pt1 =50 ;     if ( j==3) pt1 =80 ;     if ( j==4) pt1 =120 ;     if ( j==5) pt1 = 170 ;
+    if ( j==1 ) pt2 = 50 ;     if ( j==2) pt2 =80 ;     if ( j==3) pt2 =120 ;     if ( j==4) pt2 =170 ;     if ( j==5) pt2 = 10000 ;
+    
+    cout << Form("Sum :  pt hat : %d - %d : %d",  pt1, pt2, (int)(n3[j]) ) << endl;
+  }
+  
+  cout << " 30GeV : " << n1[1] <<endl;
+  cout << " 50GeV : " << n1[2] <<endl;
+  cout << " 80GeV : " << n1[3] <<endl;
+  cout << " 120GeV : " << n1[4] <<endl;
+  cout << " 170GeV : " << n1[5] <<endl;
+  
+  
   TH1::SetDefaultSumw2();
   TFile *f[10]; 
   TTree* t[10];
   TH1D* h[10];
-  double n[10];
   for ( int i=1 ; i <=5; i++) {	
     if ( i==1 )  f[i] = new TFile("yskimmedFiles/pbpb_2013fall/yskim_PbPb_pythiaHYDJET_forest_AllQCDPhotons30.root");
     else if ( i==2 )  f[i] = new TFile("yskimmedFiles/pbpb_2013fall/yskim_PbPb_pythiaHYDJET_forest_AllQCDPhotons50.root");
@@ -34,10 +87,10 @@ void cross_section_weight()
     else if ( i==5 )  f[i] = new TFile("yskimmedFiles/pbpb_2013fall/yskim_PbPb_pythiaHYDJET_forest_AllQCDPhotons170.root");
     
     t[i] = (TTree*)f[i]->Get("tgj");
-    n[i] = t[i]->GetEntries();
+    //    n[i] = t[i]->GetEntries();
     h[i] = new TH1D(Form("h_%d",i),"",300,0,300);
     t[i]->Draw(Form("photonEt>>%s",h[i]->GetName()));
-    h[i]->Scale(1./n[i]);
+    h[i]->Scale(1./n1[i]);
     handsomeTH1(h[i],i);
   }
   TCanvas *c1 = new TCanvas("c1","",800,400);
@@ -66,13 +119,13 @@ void cross_section_weight()
   cout << " 120GeV : " << w[4] <<endl;
   cout << " 170GeV : " << w[5] <<endl;
   cout << " ===============================" << endl;
-  cout << " 30 - 50GeV : " << w[1]-w[2] <<endl;
-  cout << " 50 - 80GeV : " << w[2]-w[3] <<endl;
-  cout << " 80 - 120GeV : " << w[3]-w[4] <<endl;
-  cout << " 120 - 170GeV : " << w[4]-w[5] <<endl;
-  cout << " 170GeV : " << w[5] <<endl;
+  cout << " 30 - 50GeV : " << w[1]-w[2] << "/" << n3[1] << "." << endl;
+  cout << " 50 - 80GeV : " << w[2]-w[3] << "/" << n3[2] << "." << endl;
+  cout << " 80 - 120GeV : " << w[3]-w[4] << "/" << n3[3] << "." << endl;
+  cout << " 120 - 170GeV : " << w[4]-w[5] << "/" << n3[4] << "." << endl;
+  cout << " 170GeV : " << w[5] << "/" << n3[5] << "." << endl;
 
-
+  
 
   c1->cd(2);
   for ( int i=1 ; i <=5; i++) {
@@ -84,5 +137,7 @@ void cross_section_weight()
     else h[i]->DrawCopy("same");
   }
   gPad->SetLogy();
+
+  
 
 }
