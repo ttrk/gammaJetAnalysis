07f77f4fd035dfa202a77efbd4f08c9f980e4dea
KayaUser <kaya@yahoo.com>
2015-04-09 18:11:01 -0400

    1. "eventMatcher_missing.C" must be fine. But still getting the error below :
    The lines below might hint at the cause of the crash.
    If they do not help you then please submit a bug report at
    http://root.cern.ch/bugs. Please post the ENTIRE stack trace
    from above as an attachment in addition to anything else
    that might help us fixing this issue.
    ===========================================================
    ===========================================================
    
    2. "runEventMatcher_missing.sh" now displays the date and run time info.

--- runEventMatcher_missing.sh
+++ runEventMatcher_missing.sh
@@ -1,9 +1,39 @@
 #!/bin/sh
+############################################################
+## print the command
+echo "command called =" $0 $1 $2
+## start time of the script
+date1=$(date +"%s")         # %s     seconds since 1970-01-01 00:00:00 UTC
+#date1Human=$(date -d @$date1)
+date1Human=$(date -d @$date1 '+%d/%m/%Y %H:%M:%S')
+echo "started on     =" $date1Human
+echo "############################################################"
+############################################################
 
 #http://stackoverflow.com/questions/3598664/creating-a-shell-script-to-run-java-program?rq=1
 
 progName="eventMatcher_missing";
 
-g++ $progName.C $(root-config --cflags --libs) -Wall -Wextra -O2 -o $progName.exe
+#g++ $progName.C $(root-config --cflags --libs) -Wall -Wextra -O2 -o $progName.exe || exit 1
+g++ $progName.C $(root-config --cflags --libs) -Wall -Wextra -g -o $progName.exe || exit 1
 
 ./$progName.exe
+
+############################################################
+echo "############################################################"
+echo "##### log of the SCRIPT =" $0
+
+## finish time and run time of the script
+date2=$(date +"%s")         # %s     seconds since 1970-01-01 00:00:00 UTC
+#date2Human=$(date -d @$date2)
+date2Human=$(date -d @$date2 '+%d/%m/%Y %H:%M:%S')
+diff=$(($date2-$date1))
+diffHuman=$(date -d @$diff)
+#echo $date1
+echo "started  on    =" $date1Human
+echo "finished on    =" $date2Human
+echo "$(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
+echo $diffHuman
+
+echo "##### END #####"
+############################################################
