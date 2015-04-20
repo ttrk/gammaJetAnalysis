fb798d35fa4de635020cc7fbc2f2238d08185c3c
KayaUser <kaya@yahoo.com>
2015-04-20 17:23:40 -0400

    // look for events after isolation cut in Forest 	// check more HLT triggers 	// in the end create a tree
    The complete list of photon triggers for the pA run is this:
    
    HLT_PAPhoton10_NoCaloIdVL_v1
    HLT_PAPhoton15_NoCaloIdVL_v1
    HLT_PAPhoton20_NoCaloIdVL_v1
    HLT_PAPhoton30_NoCaloIdVL_v1
    HLT_PAPhoton40_NoCaloIdVL_v1
    HLT_PAPhoton60_NoCaloIdVL_v1
    HLT_PAPhoton10_TightCaloIdVL_v1
    HLT_PAPhoton15_TightCaloIdVL_v1
    HLT_PAPhoton20_TightCaloIdVL_v1
    HLT_PAPhoton30_TightCaloIdVL_v1
    HLT_PAPhoton40_TightCaloIdVL_v1
    HLT_PAPhoton10_TightCaloIdVL_Iso50_v1
    HLT_PAPhoton15_TightCaloIdVL_Iso50_v1
    HLT_PAPhoton20_TightCaloIdVL_Iso50_v1
    HLT_PAPhoton30_TightCaloIdVL_Iso50_v1
    HLT_PAPhoton10_Photon10_NoCaloIdVL_v1
    HLT_PAPhoton15_Photon10_NoCaloIdVL_v1
    HLT_PAPhoton20_Photon15_NoCaloIdVL_v1
    HLT_PAPhoton20_Photon20_NoCaloIdVL_v1
    HLT_PAPhoton30_Photon30_NoCaloIdVL_v1
    HLT_PAPhoton10_Photon10_TightCaloIdVL_v1
    HLT_PAPhoton10_Photon10_TightCaloIdVL_Iso50_v1
    HLT_PAPhoton15_Photon10_TightCaloIdVL_v1
    HLT_PAPhoton20_Photon15_TightCaloIdVL_v1
    
    The 58 isolated photon > 40GeV events missing from the new sample which do not fire any of the single NoCaloID triggers must have fired one of the other triggers. I had assumed that the TightCaloId triggers would be subsets of the NoCaloID triggers, but maybe a prescale difference means that these 58 events could be in the Tight or double-photon triggers.
    
    Since we're spending some time looking at these events, how about for all the isolated photon > 40 GeV events you make a TTree of the Event/Lumi/Run and these trigger results? Oh, and also include an extra boolean, "isInNewSample" or something like that.
    
    Alex

--- eventMatcher_missing.C
+++ eventMatcher_missing.C
@@ -40,7 +40,11 @@ const bool plot_sigmaIetaIeta    = false;	// true : plot the sigmaIetaIeta distr
 											// this implied there could be missing events in the new sample
 											// https://twiki.cern.ch/twiki/pub/CMS/PhotonAnalyses2015/150407_forest2yskim.pdf, slide 9
 
-const bool check_forest = true ;			// look for missing events in Forest
+const bool check_forest = false ;					// look for missing events in Forest
+const bool check_forest_hltTree = true ;			// look for events after isolation cut in Forest
+													// check more HLT triggers
+													// in the end create a tree
+const TString outFileName_hltTriggerTree = "/net/hisrv0001/home/tatar/output/out_gammaJetAnalysis/eventMatcher_missing_forest_v2.root";
 
 // 34342
 // 33635
@@ -49,6 +53,12 @@ int eventMatcher_missing_forest(TString inputFile_forest,
 				   EventMatchingCMS* eventMatcherYSKIM,
 				   int hlt_triggers_fired[3],
 				   sampleType colli=kPADATA
+				   );
+
+int eventMatcher_missing_forest_v2(TString inputFile_forest, TString outputFileName,
+				   EventMatchingCMS* eventMatcher2,
+				   EventMatchingCMS* eventsNotInNewSample,
+				   sampleType colli /* =kPADATA */
 				   );
 
 void eventMatcher_missing(const TString fileName1, const TString fileName2)
@@ -342,6 +352,37 @@ void eventMatcher_missing(const TString fileName1, const TString fileName2)
 		  cout << "HLT_PAPhoton30_NoCaloIdVL_v1_count = " << HLT_PAPhoton30_NoCaloIdVL_v1_count << endl;
 		  cout << "HLT_PAPhoton40_NoCaloIdVL_v1_count = " << HLT_PAPhoton40_NoCaloIdVL_v1_count << endl;
 	  }
+
+	  if(check_forest_hltTree)
+	  {
+		  // recreate the output file
+		  TString outFileName = outFileName_hltTriggerTree;
+		  TFile* outFile = new TFile(outFileName_hltTriggerTree, "RECREATE");
+		  outFile->Close();
+
+		  TString inputForest_name;
+		  int match_count     = 0;
+		  int match_count_tmp = 0;
+		  const char* inputForest_prefix = "/mnt/hadoop/cms/store/user/luck/pA_photonSkimForest_v85_fromPromptReco_partialmerge";
+		  for (int i=0; i<=89; i++)	// forest file names go from 0.root to 89.root
+		  {
+			  inputForest_name = Form("%s/%d.root",inputForest_prefix,i);
+			  match_count_tmp  = eventMatcher_missing_forest_v2(inputForest_name, outFileName, eventMatcher2, eventsNotInFile1, kPADATA);
+			  match_count     += match_count_tmp;
+		  }
+
+		  cout << "events in new sample = " << entries1 << endl;
+		  cout << "events in old sample = " << entries2 << endl;
+		  cout << "Duplicate events in new sample = " << duplicateEvents1 << endl;
+		  cout << "Duplicate events in old sample = " << duplicateEvents2 << endl;
+
+		  cout << "events in new sample that pass isolation cut = "    << entries1_after_iso << endl;
+		  cout << "events in old sample that pass isolation cut = "    << entries2_after_iso << endl;
+		  cout << "events in old sample that are not in new sample = " << notRetrievedEvents << endl;
+
+		  cout << "number of isolation cut passing events matched in the whole forest = " << match_count      << endl;
+		  cout << "hltTriggerTree has been written to : " << outFileName << endl;
+	  }
 }
 
 /*
@@ -452,6 +493,242 @@ int eventMatcher_missing_forest(TString inputFile_forest,
   cout << "number of events matched = " << match_count      << endl;
   cout << "closing file             = " << inputFile_forest << endl;
 
+  c->inf->Close();
+
+  return match_count;
+}
+
+/*
+	Kaya,
+
+The complete list of photon triggers for the pA run is this:
+
+HLT_PAPhoton10_NoCaloIdVL_v1
+HLT_PAPhoton15_NoCaloIdVL_v1
+HLT_PAPhoton20_NoCaloIdVL_v1
+HLT_PAPhoton30_NoCaloIdVL_v1
+HLT_PAPhoton40_NoCaloIdVL_v1
+HLT_PAPhoton60_NoCaloIdVL_v1
+HLT_PAPhoton10_TightCaloIdVL_v1
+HLT_PAPhoton15_TightCaloIdVL_v1
+HLT_PAPhoton20_TightCaloIdVL_v1
+HLT_PAPhoton30_TightCaloIdVL_v1
+HLT_PAPhoton40_TightCaloIdVL_v1
+HLT_PAPhoton10_TightCaloIdVL_Iso50_v1
+HLT_PAPhoton15_TightCaloIdVL_Iso50_v1
+HLT_PAPhoton20_TightCaloIdVL_Iso50_v1
+HLT_PAPhoton30_TightCaloIdVL_Iso50_v1
+HLT_PAPhoton10_Photon10_NoCaloIdVL_v1
+HLT_PAPhoton15_Photon10_NoCaloIdVL_v1
+HLT_PAPhoton20_Photon15_NoCaloIdVL_v1
+HLT_PAPhoton20_Photon20_NoCaloIdVL_v1
+HLT_PAPhoton30_Photon30_NoCaloIdVL_v1
+HLT_PAPhoton10_Photon10_TightCaloIdVL_v1
+HLT_PAPhoton10_Photon10_TightCaloIdVL_Iso50_v1
+HLT_PAPhoton15_Photon10_TightCaloIdVL_v1
+HLT_PAPhoton20_Photon15_TightCaloIdVL_v1
+
+The 58 isolated photon > 40GeV events missing from the new sample which do not fire any of the single NoCaloID triggers must have fired one of the other triggers. I had assumed that the TightCaloId triggers would be subsets of the NoCaloID triggers, but maybe a prescale difference means that these 58 events could be in the Tight or double-photon triggers.
+
+Since we're spending some time looking at these events, how about for all the isolated photon > 40 GeV events you make a TTree of the Event/Lumi/Run and these trigger results? Oh, and also include an extra boolean, "isInNewSample" or something like that.
+
+Alex
+ *
+ * */
+int eventMatcher_missing_forest_v2(TString inputFile_forest, TString outputFileName,
+				   EventMatchingCMS* eventMatcher2,
+				   EventMatchingCMS* eventsNotInNewSample,
+				   sampleType colli /* =kPADATA */
+				   )
+{
+  bool isMC=true;
+  if ((colli==kPPDATA)||(colli==kPADATA)||(colli==kHIDATA))
+    isMC=false;
+
+  HiForest *c;
+  if((colli==kPADATA)||(colli==kPAMC)) {
+    c = new HiForest(inputFile_forest.Data(), "forest", cPPb, isMC );
+  }
+  else if  ((colli==kPPDATA)||(colli==kPPMC)) {
+    c = new HiForest(inputFile_forest.Data(), "forest", cPP, isMC );
+  }
+  else if  ((colli==kHIDATA)||(colli==kHIMC)) {
+    c = new HiForest(inputFile_forest.Data(), "forest", cPbPb, isMC );
+    c->GetEnergyScaleTable("../photonEnergyScaleTable_lowPt_v6.root");
+  }
+  else {
+    cout << " Error!  No such collision type" << endl;
+    return -1;
+  }
+
+  //////// Kaya's modificiation ////////
+  cout << "inputFile_forest         = " << inputFile_forest		<< endl;
+  //////// Kaya's modificiation - END ////////
+
+  c->LoadNoTrees();
+  c->hasEvtTree = true;
+  c->hasHltTree = true;
+
+  c->InitTree();
+
+  TFile* outFile=new TFile(outputFileName, "UPDATE");
+
+  int missingFromNewSample;
+  TTree* hltTriggerTree;
+  const char* branchNames[]={
+		  "HLT_PAPhoton10_NoCaloIdVL_v1",
+		  "HLT_PAPhoton15_NoCaloIdVL_v1",
+		  "HLT_PAPhoton20_NoCaloIdVL_v1",
+		  "HLT_PAPhoton30_NoCaloIdVL_v1",
+		  "HLT_PAPhoton40_NoCaloIdVL_v1",
+		  "HLT_PAPhoton60_NoCaloIdVL_v1",
+		  "HLT_PAPhoton10_TightCaloIdVL_v1",
+		  "HLT_PAPhoton15_TightCaloIdVL_v1",
+		  "HLT_PAPhoton20_TightCaloIdVL_v1",
+		  "HLT_PAPhoton30_TightCaloIdVL_v1",
+		  "HLT_PAPhoton40_TightCaloIdVL_v1",
+		  "HLT_PAPhoton10_TightCaloIdVL_Iso50_v1",
+		  "HLT_PAPhoton15_TightCaloIdVL_Iso50_v1",
+		  "HLT_PAPhoton20_TightCaloIdVL_Iso50_v1",
+		  "HLT_PAPhoton30_TightCaloIdVL_Iso50_v1",
+		  "HLT_PAPhoton10_Photon10_NoCaloIdVL_v1",
+		  "HLT_PAPhoton15_Photon10_NoCaloIdVL_v1",
+		  "HLT_PAPhoton20_Photon15_NoCaloIdVL_v1",
+		  "HLT_PAPhoton20_Photon20_NoCaloIdVL_v1",
+		  "HLT_PAPhoton30_Photon30_NoCaloIdVL_v1",
+		  "HLT_PAPhoton10_Photon10_TightCaloIdVL_v1",
+		  "HLT_PAPhoton10_Photon10_TightCaloIdVL_Iso50_v1",
+		  "HLT_PAPhoton15_Photon10_TightCaloIdVL_v1",
+		  "HLT_PAPhoton20_Photon15_TightCaloIdVL_v1"
+  };
+
+  if(outFile->GetListOfKeys()->Contains("hltTriggerTree"))
+  {
+	  hltTriggerTree=(TTree*)outFile->Get("hltTriggerTree");
+
+	  hltTriggerTree->SetBranchAddress("evt",  &c->evt.evt);
+	  hltTriggerTree->SetBranchAddress("lumi", &c->evt.lumi);
+	  hltTriggerTree->SetBranchAddress("run",  &c->evt.run);
+	  hltTriggerTree->SetBranchAddress("missingFromNewSample", &missingFromNewSample);
+
+	  hltTriggerTree->SetBranchAddress(branchNames[0], &c->hlt.HLT_PAPhoton10_NoCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[1], &c->hlt.HLT_PAPhoton15_NoCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[2], &c->hlt.HLT_PAPhoton20_NoCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[3], &c->hlt.HLT_PAPhoton30_NoCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[4], &c->hlt.HLT_PAPhoton40_NoCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[5], &c->hlt.HLT_PAPhoton60_NoCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[6], &c->hlt.HLT_PAPhoton10_TightCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[7], &c->hlt.HLT_PAPhoton15_TightCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[8], &c->hlt.HLT_PAPhoton20_TightCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[9], &c->hlt.HLT_PAPhoton30_TightCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[10], &c->hlt.HLT_PAPhoton40_TightCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[11], &c->hlt.HLT_PAPhoton10_TightCaloIdVL_Iso50_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[12], &c->hlt.HLT_PAPhoton15_TightCaloIdVL_Iso50_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[13], &c->hlt.HLT_PAPhoton20_TightCaloIdVL_Iso50_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[14], &c->hlt.HLT_PAPhoton30_TightCaloIdVL_Iso50_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[15], &c->hlt.HLT_PAPhoton10_Photon10_NoCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[16], &c->hlt.HLT_PAPhoton15_Photon10_NoCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[17], &c->hlt.HLT_PAPhoton20_Photon15_NoCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[18], &c->hlt.HLT_PAPhoton20_Photon20_NoCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[19], &c->hlt.HLT_PAPhoton30_Photon30_NoCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[20], &c->hlt.HLT_PAPhoton10_Photon10_TightCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[21], &c->hlt.HLT_PAPhoton10_Photon10_TightCaloIdVL_Iso50_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[22], &c->hlt.HLT_PAPhoton15_Photon10_TightCaloIdVL_v1);
+	  hltTriggerTree->SetBranchAddress(branchNames[23], &c->hlt.HLT_PAPhoton20_Photon15_TightCaloIdVL_v1);
+  }
+  else
+  {
+	  hltTriggerTree=new TTree("hltTriggerTree", "subset of HLT photon triggers ");
+//	  hltTriggerTree->Branch("HLT_PAPhoton10_NoCaloIdVL_v1", &c->hlt.HLT_PAPhoton10_NoCaloIdVL_v1, Form("%s/I","HLT_PAPhoton10_NoCaloIdVL_v1"));
+	  hltTriggerTree->Branch("evt",  &c->evt.evt,  Form("%s/I", "evt"));
+	  hltTriggerTree->Branch("lumi", &c->evt.lumi, Form("%s/I", "lumi"));
+	  hltTriggerTree->Branch("run",  &c->evt.run,  Form("%s/I", "run"));
+	  hltTriggerTree->Branch("missingFromNewSample", &missingFromNewSample,Form("%s/I","missingFromNewSample"));
+
+	  hltTriggerTree->Branch(branchNames[0], &c->hlt.HLT_PAPhoton10_NoCaloIdVL_v1, Form("%s/I",branchNames[0]));
+	  hltTriggerTree->Branch(branchNames[1], &c->hlt.HLT_PAPhoton15_NoCaloIdVL_v1, Form("%s/I",branchNames[1]));
+	  hltTriggerTree->Branch(branchNames[2], &c->hlt.HLT_PAPhoton20_NoCaloIdVL_v1, Form("%s/I",branchNames[2]));
+	  hltTriggerTree->Branch(branchNames[3], &c->hlt.HLT_PAPhoton30_NoCaloIdVL_v1, Form("%s/I",branchNames[3]));
+	  hltTriggerTree->Branch(branchNames[4], &c->hlt.HLT_PAPhoton40_NoCaloIdVL_v1, Form("%s/I",branchNames[4]));
+	  hltTriggerTree->Branch(branchNames[5], &c->hlt.HLT_PAPhoton60_NoCaloIdVL_v1, Form("%s/I",branchNames[5]));
+	  hltTriggerTree->Branch(branchNames[6], &c->hlt.HLT_PAPhoton10_TightCaloIdVL_v1, Form("%s/I",branchNames[6]));
+	  hltTriggerTree->Branch(branchNames[7], &c->hlt.HLT_PAPhoton15_TightCaloIdVL_v1, Form("%s/I",branchNames[7]));
+	  hltTriggerTree->Branch(branchNames[8], &c->hlt.HLT_PAPhoton20_TightCaloIdVL_v1, Form("%s/I",branchNames[8]));
+	  hltTriggerTree->Branch(branchNames[9], &c->hlt.HLT_PAPhoton30_TightCaloIdVL_v1, Form("%s/I",branchNames[9]));
+	  hltTriggerTree->Branch(branchNames[10], &c->hlt.HLT_PAPhoton40_TightCaloIdVL_v1, Form("%s/I",branchNames[10]));
+	  hltTriggerTree->Branch(branchNames[11], &c->hlt.HLT_PAPhoton10_TightCaloIdVL_Iso50_v1, Form("%s/I",branchNames[11]));
+	  hltTriggerTree->Branch(branchNames[12], &c->hlt.HLT_PAPhoton15_TightCaloIdVL_Iso50_v1, Form("%s/I",branchNames[12]));
+	  hltTriggerTree->Branch(branchNames[13], &c->hlt.HLT_PAPhoton20_TightCaloIdVL_Iso50_v1, Form("%s/I",branchNames[13]));
+	  hltTriggerTree->Branch(branchNames[14], &c->hlt.HLT_PAPhoton30_TightCaloIdVL_Iso50_v1, Form("%s/I",branchNames[14]));
+	  hltTriggerTree->Branch(branchNames[15], &c->hlt.HLT_PAPhoton10_Photon10_NoCaloIdVL_v1, Form("%s/I",branchNames[15]));
+	  hltTriggerTree->Branch(branchNames[16], &c->hlt.HLT_PAPhoton15_Photon10_NoCaloIdVL_v1, Form("%s/I",branchNames[16]));
+	  hltTriggerTree->Branch(branchNames[17], &c->hlt.HLT_PAPhoton20_Photon15_NoCaloIdVL_v1, Form("%s/I",branchNames[17]));
+	  hltTriggerTree->Branch(branchNames[18], &c->hlt.HLT_PAPhoton20_Photon20_NoCaloIdVL_v1, Form("%s/I",branchNames[18]));
+	  hltTriggerTree->Branch(branchNames[19], &c->hlt.HLT_PAPhoton30_Photon30_NoCaloIdVL_v1, Form("%s/I",branchNames[19]));
+	  hltTriggerTree->Branch(branchNames[20], &c->hlt.HLT_PAPhoton10_Photon10_TightCaloIdVL_v1,       Form("%s/I",branchNames[20]));
+	  hltTriggerTree->Branch(branchNames[21], &c->hlt.HLT_PAPhoton10_Photon10_TightCaloIdVL_Iso50_v1, Form("%s/I",branchNames[21]));
+	  hltTriggerTree->Branch(branchNames[22], &c->hlt.HLT_PAPhoton15_Photon10_TightCaloIdVL_v1,       Form("%s/I",branchNames[22]));
+	  hltTriggerTree->Branch(branchNames[23], &c->hlt.HLT_PAPhoton20_Photon15_TightCaloIdVL_v1,       Form("%s/I",branchNames[23]));
+  }
+
+  // Loop starts.
+  int nentries = c->GetEntries();
+  cout << "number of entries = " << nentries << endl;
+  long long    eventRetrieved;
+  long long    eventRetrievedNotInNewSample;
+  int match_count = 0;
+  int match_count_missingFromNewSample = 0;
+
+  for (Long64_t jentry = 0 ; jentry < nentries; jentry++) {
+
+    if (jentry% 200000 == 0)  {
+      cout <<jentry<<" / "<<nentries<<" "<<setprecision(2)<<(double)jentry/nentries*100<<endl;
+    }
+
+    c->GetEntry(jentry);
+
+    eventRetrieved = eventMatcher2->retrieveEventNoErase(c->evt.evt, 0, c->evt.run);	// eventMatcher2 is not created using lumi values
+    																					// "evt" and "run" should be enough to match the events.
+    if(eventRetrieved>0)	// this event is inside eventMatcher2
+    {
+    	match_count++;
+
+    	// this method will catch around 34000 events, so better do not print anything to stdout for those events.
+
+    	// now check whether this event is also missing from new sample
+    	eventRetrievedNotInNewSample = eventsNotInNewSample->retrieveEventNoErase(c->evt.evt, 0, c->evt.run);
+    	if(eventRetrievedNotInNewSample > 0)
+    	{
+    		match_count_missingFromNewSample++;
+
+    		missingFromNewSample=1;
+    	}
+    	else
+    	{
+    		missingFromNewSample=0;
+    	}
+
+    	hltTriggerTree->Fill();
+    }
+  }
+
+  cout << "number of events matched = "                   << match_count    					<< endl;
+  cout << "number of events matches not in new sample = " << match_count_missingFromNewSample   << endl;
+  cout << "closing file             = " 				  << inputFile_forest << endl;
+
+  outFile->cd();
+
+  //  hltTriggerTree->Write();
+  // save only the new version of the tree
+/*
+ *	https://root.cern.ch/root/html/TTree
+  Note the kOverwrite option in the Write method, it overwrites the
+  existing tree. If it is not specified, two copies of the tree headers
+  are saved.
+  */
+  hltTriggerTree->Write("", TObject::kOverwrite);		// https://root.cern.ch/root/roottalk/roottalk02/2302.html
+
+  outFile->Close();
   c->inf->Close();
 
   return match_count;
