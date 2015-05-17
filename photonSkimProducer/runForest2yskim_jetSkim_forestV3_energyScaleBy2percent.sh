0fd5bbc4dbb874a2e8be91f0b98beea86d35b071
Yeonju Go <yeonjugo@gmail.com>
2015-05-17 18:51:12 +0900

    including jetEnergyScale in yskim, turn on the electron rejection in default yskim, Event matching added

--- photonSkimProducer/runForest2yskim_jetSkim_forestV3_energyScaleBy2percent.sh
+++ photonSkimProducer/runForest2yskim_jetSkim_forestV3_energyScaleBy2percent.sh
@@ -14,13 +14,13 @@
 #                       0    1      2       3       4        5 
 
 # pbpb data
-root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pbpb/HiForestPhoton-v7-noDuplicate.root",       "yskimmedFiles/minbiasSkim_HIMinBias_merged_1_1000.root",    40,  "yskim_HiForestPhoton-v7-noDuplicate_jetScaleBy2percent.root",  0, 1, 0, 0, 0,1.02)'
-root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pbpb/HiForestPhoton-v7-noDuplicate.root",       "yskimmedFiles/minbiasSkim_HIMinBias_merged_1_1000.root",    40,  "yskim_HiForestPhoton-v7-noDuplicate_jetScaleBy-2percent.root",  0, 1, 0, 0, 0,0.98)'
+root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pbpb/HiForestPhoton-v7-noDuplicate.root",       "yskimmedFiles/minbiasSkim_HIMinBias_merged_1_1000.root",    40,  "yskim_HiForestPhoton-v7-noDuplicate_jetScaleBy2percent.root",  0, 1, 1,-1,1.02)'
+root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pbpb/HiForestPhoton-v7-noDuplicate.root",       "yskimmedFiles/minbiasSkim_HIMinBias_merged_1_1000.root",    40,  "yskim_HiForestPhoton-v7-noDuplicate_jetScaleBy-2percent.root",  0, 1, 1,-1.,0.98)'
 
 # pp data 
-root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pp/pp_photonSKimForest_v85.root",       "no minbias", 40,   "yskim_pp_photonSKimForest_v85_jetScaleBy2percent.root",2,0, 0, 0,0, 1.02)'
-root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pp/pp_photonSKimForest_v85.root",       "no minbias", 40,   "yskim_pp_photonSKimForest_v85_jetScaleBy-2percent.root",2,0, 0, 0,0, 0.98)'
+root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pp/pp_photonSKimForest_v85.root",       "no minbias", 40,   "yskim_pp_photonSKimForest_v85_jetScaleBy2percent.root",2,0, 1,-1, 1.02)'
+root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pp/pp_photonSKimForest_v85.root",       "no minbias", 40,   "yskim_pp_photonSKimForest_v85_jetScaleBy-2percent.root",2,0, 1,-1, 0.98)'
 
 #  pA data, photon hiForest 
-root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pA/pA_photonSkimForest_v85_HLT_PAPhoton30_NoCaloIdVL_v1_highPtPhoton40.root", "", 40,  "yskim_pA_photonSkimForest_v85_HLT_PAPhoton30_NoCaloIdVL_v1_highPtPhoton40_jetScaleBy2percent.root",4,0, 0, 0, 0,1.02)'
-root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pA/pA_photonSkimForest_v85_HLT_PAPhoton30_NoCaloIdVL_v1_highPtPhoton40.root",   "", 40,  "yskim_pA_photonSkimForest_v85_HLT_PAPhoton30_NoCaloIdVL_v1_highPtPhoton40_jetScaleBy-2percent.root",4,0, 0, 0, 0,0.98)'
+root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pA/pA_photonSkimForest_v85_HLT_PAPhoton30_NoCaloIdVL_v1_highPtPhoton40.root", "", 40,  "yskim_pA_photonSkimForest_v85_HLT_PAPhoton30_NoCaloIdVL_v1_highPtPhoton40_jetScaleBy2percent.root",4,0, 1,-1,1.02)'
+root -l -q -b 'forest2yskim_jetSkim_forestV3.C+("forestFiles/pA/pA_photonSkimForest_v85_HLT_PAPhoton30_NoCaloIdVL_v1_highPtPhoton40.root",   "", 40,  "yskim_pA_photonSkimForest_v85_HLT_PAPhoton30_NoCaloIdVL_v1_highPtPhoton40_jetScaleBy-2percent.root",4,0, 1,-1,0.98)'
